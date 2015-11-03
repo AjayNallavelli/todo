1c068c398bdae129d71ae3215e35dbac8751f386
Pooja Saxena <pooja335@gmail.com>
2015-11-03 01:22:04 -0600

    we can get our location

--- app/xlist/scripts/HomeController.js
+++ app/xlist/scripts/HomeController.js
@@ -2,13 +2,24 @@ angular
   .module('xlist')
   .controller('HomeController', function($scope, supersonic) {
 
+    $scope.withinDistance = false;
+    $scope.wholefoods = {latitude: '', longitude: '', timestamp: ''};
+    $scope.wholefoods.latitude = 42.046858;
+    $scope.wholefoods.longitude = -87.679596;
     $scope.mylocation = {latitude: '', longitude: '', timestamp: ''};
-    supersonic.device.geolocation.getPosition().then(function(position) {
-      mylocation.latitude = position.coords.latitude;
-      mylocation.longitude = position.coords.longitude;
-      mylocation.timestamp = position.timestamp;
-    });
 
-    console.log(mylocation);
+    var getDistance = function() {
+      setInterval(function(){
+        supersonic.device.geolocation.getPosition().then(function(position) {
+          $scope.mylocation.latitude = position.coords.latitude;
+          $scope.mylocation.longitude = position.coords.longitude;
+          $scope.mylocation.timestamp = position.timestamp;
+        });
+        if ((Math.abs($scope.wholefoods.latitude - $scope.mylocation.latitude) < 1) && (Math.abs($scope.wholefoods.longitude - $scope.mylocation.longitude) < 1)) {
+          $scope.withinDistance = true;
+        }
+      }, 1000);
+    };
+    getDistance();
 
   });
