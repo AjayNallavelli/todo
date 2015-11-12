accc49ff64261ec3b74f68ede1defbaca8f4d558
Pooja Saxena <pooja335@gmail.com>
2015-11-12 03:35:37 -0600

    button and minor ui change - temporary but i wanna sleeeeep

--- app/xlist/scripts/LocationController.js
+++ app/xlist/scripts/LocationController.js
@@ -5,10 +5,13 @@ angular
        'push', 'ParseQuery', 'ParseObject', 'uiGmapGoogleMapApi',
   function($scope, $q, supersonic, Task, Store, deviceReady, slackbot, push, ParseQuery, ParseObject, uiGmapGoogleMapApi) {
    
-    $scope.map = { center: { latitude: 42.0464, longitude: -87.6947 }, zoom: 12 };
-    var events = {
-      places_changed: function (searchBox) {}
-    }
-    $scope.searchbox = { template:'searchbox.tpl.html', events:events};
+    var makeMap = function() {
+      $scope.map = { center: { latitude: 42.0464, longitude: -87.6947 }, zoom: 12 };
+      var events = {
+        places_changed: function (searchBox) {}
+      }
+      $scope.searchbox = { template:'searchbox.tpl.html', events:events};
+    };
+    makeMap();
 
   }]);
