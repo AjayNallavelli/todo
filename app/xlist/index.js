c0328127d70064c890aea4443d0a15bc20852517
Adrien Katsuya Tateno <adrien.k.tateno@gmail.com>
2015-11-12 18:46:10 -0600

    DI style

--- app/xlist/index.js
+++ app/xlist/index.js
@@ -1,13 +1,13 @@
-angular.module('xlist', [
-  // Declare here all AngularJS dependencies that are shared by the example module.
-  'supersonic',
-  'ParseServices',
-  'uiGmapgoogle-maps'
-])
-.config(function(uiGmapGoogleMapApiProvider) {
-  uiGmapGoogleMapApiProvider.configure({
-    key: 'AIzaSyBIQ2D3GaqQEbNIh3HbzEcf-fdYjR5r6So',
-    v: '3.20', //defaults to latest 3.X anyhow
-    libraries: 'places'
-  });
-});
+angular
+  .module('xlist', [
+    // Declare here all AngularJS dependencies that are shared by the example module.
+    'supersonic',
+    'uiGmapgoogle-maps'
+  ])
+  .config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+      key: 'AIzaSyBIQ2D3GaqQEbNIh3HbzEcf-fdYjR5r6So',
+      v: '3.20', //defaults to latest 3.X anyhow
+      libraries: 'places'
+    });
+  }]);
