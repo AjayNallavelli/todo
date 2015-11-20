4ff98cbf3fd72d867b127957bff1dc253886bb01
LunrongChen <lunrongchen@163.com>
2015-11-19 20:17:24 -0600

    getgeolist final

--- app/xlist/scripts/GeolistController.js
+++ app/xlist/scripts/GeolistController.js
@@ -10,25 +10,16 @@ angular
 
     var getGeolists = function() {
       new Parse.Query(GeoList).find().then(function(results) {
-        _.each(results, function(result)){
-          $scope.geolists.push(new ParseObject(result, Geolist.fields))
-        }
+        _.each(results, function(result){
+          $scope.$apply(function($scope) {
+            $scope.geolists.push(new ParseObject(result, GeoList.fields));
+          });
+          // console.log(result);
+        });
+        console.log($scope.geolists);
       });
     };
 
-    var deleteTasks = function(geoList) {
-      var deferred = $q.defer();
-      var queryTasks = new Parse.Query(Task);
-      queryTasks.equalTo('geoList', geoList.toPointer()).find()
-          .then(function(results) {
-            var tasks = [];
-            for (var i = 0; i < results.length; i++) {
-              tasks[i].geoList = null;
-              tasks.push(new ParseObject(results[i], Task.fields));
-            }
-            deferred.resolve(tasks);
-          }, alertParseError);
-    };
-
     supersonic.ui.views.current.whenVisible(getGeolists);
+
   }]);
