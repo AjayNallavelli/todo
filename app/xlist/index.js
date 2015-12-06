7a7060fa1c414e8cbc993c64cbb4c2feabb61515
Kapil Arun Garg 3 <kgarg6@sapient.com>
2015-12-06 17:30:20 -0600

    removing maps API key

--- app/xlist/index.js
+++ app/xlist/index.js
@@ -7,7 +7,7 @@ angular
   ])
   .config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-      key: 'AIzaSyBIQ2D3GaqQEbNIh3HbzEcf-fdYjR5r6So',
+      key: 'INSERT GOOGLE MAPS API KEY HERE',
       v: '3.20', //defaults to latest 3.X anyhow
       libraries: 'places'
     });
